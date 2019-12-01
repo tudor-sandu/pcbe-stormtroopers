d0c833be13a88e8781d39cf674eb1e4376f364c2
Sandu Tudor <tudor.sandu@3pillarglobal.com>
2019-12-01 19:48:23 +0200

    clean slate

--- src/com/bursagalactica/Cumparator.java
+++
@@ -1,9 +0,0 @@
-package com.bursagalactica;
-
-public class Cumparator {
-    public void cumpara(Actiune e, Bursa b) {
-        new Thread(() -> {
-            b.cumpara(e);
-        }).start();
-    }
-}
--- src/com/bursagalactica/Vanzator.java
+++
@@ -1,9 +0,0 @@
-package com.bursagalactica;
-
-public class Vanzator {
-    public void vinde(Actiune e, Bursa b) {
-        new Thread(() -> {
-            b.vinde(e);
-        }).start();
-    }
-}
